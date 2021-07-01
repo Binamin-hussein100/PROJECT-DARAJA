6f9734d685d13d84664272b92a8f3aae054078e1
keithkaykay <kaykaykeith@yahoo.com>
2021-07-01 23:21:17 +0300

    create javascript function for login pages

--- js-kk/script.js
+++ js-kk/script.js
@@ -1,6 +1,18 @@
-window.onclick = function(event) {
-  if (event.target == modal) {
-      modal.style.display = "none";
-  }
+function loginadmin(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  alert("Welcome " + username);
+
+}
+
+function getUsername() {
+  var username = document.getElementById("username").value;
+  return username;
+}
+
+function getPassword() {
+  var password = document.getElementById("password").value;
+  return password;
 }
 
